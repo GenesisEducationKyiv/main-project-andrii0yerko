0ff78d693f931674971606420318064f7536a590
Andrii Yerko <aerko12@gmail.com>
2023-08-01 09:41:32 +0300

    style: rename unused argument

--- pkg/app/ratehandler.go
+++ pkg/app/ratehandler.go
@@ -8,7 +8,7 @@ import (
 
 // Return current exchange rate
 // Accepts GET, returns rate value and StatusOK
-func (e ExchangeRateHandler) GetRate(w http.ResponseWriter, r *http.Request) {
+func (e ExchangeRateHandler) GetRate(w http.ResponseWriter, _ *http.Request) {
 	log.Printf("got GetRate request\n")
 	value, err := e.rateService.ExchangeRate()
 	if err != nil {
@@ -33,7 +33,7 @@ func (e ExchangeRateHandler) GetRate(w http.ResponseWriter, r *http.Request) {
 
 // Send email with current rate to all the subscribers
 // Accepts POST, returns StatusOK if the email was not subscribed before and StatusConflict otherwise
-func (e ExchangeRateHandler) PostSendEmails(w http.ResponseWriter, r *http.Request) {
+func (e ExchangeRateHandler) PostSendEmails(w http.ResponseWriter, _ *http.Request) {
 	log.Printf("got PostSendEmails request\n")
 	err := e.rateService.Notify()
 	if err != nil {
